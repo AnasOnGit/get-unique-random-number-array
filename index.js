6b4f5c1a05a7282205b18f22da33e6be1136ef8a
AnasOnGit <50570153+AnasOnGit@users.noreply.github.com>
2022-02-18 22:32:45 +0300

    Feature added: Only return unique arrays: EG:- [2,1] - [1,2] are same, so it will only return [2,1]. and other improvements

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const { isArraySame } = require("is-array-same");
+
+// helper function
+const isArrayUnique = (array, check) => {
+    return array.some((array) => {
+        return isArraySame(array, check);
+    });
+};
+
+//  main function
 const uniqueMultiRandomIndexFromArray = (
     array,
     number,
@@ -6,11 +16,37 @@ const uniqueMultiRandomIndexFromArray = (
     let uniqueIndex = [];
     while (uniqueIndex.length < number) {
         const randomIndex = Math.floor(Math.random() * array.length);
-        if (previousUniqueIndex.indexOf(randomIndex) === -1) {
+        if (uniqueIndex.length < 1) {
             uniqueIndex.push(randomIndex);
+        } else {
+            let tmp = [...uniqueIndex];
+            tmp.push(randomIndex);
+
+            if (!isArrayUnique(previousUniqueIndex, tmp) &&
+                !isArrayUnique(previousUniqueIndex, tmp.reverse())
+            ) {
+                uniqueIndex.push(randomIndex);
+            }
         }
     }
     return uniqueIndex;
+};
+
+// Function to test above function
+const test = () => {
+    const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const number = 2;
+    const previousUniqueIndex = [];
+    // loop
+    for (let i = 0; i < 10; i++) {
+        const uniqueIndex = uniqueMultiRandomIndexFromArray(
+            array,
+            number,
+            previousUniqueIndex
+        );
+        previousUniqueIndex.push(uniqueIndex);
+    }
+    return previousUniqueIndex;
 };
 
 module.exports = {
